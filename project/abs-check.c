1f256f9cb92f888e7d13bc922b3cfe39be8bf68e
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2022-05-29 21:56:27 -0400

    Example for verification by CBMC

---
+++ project/abs-check.c
@@ -0,0 +1,17 @@
+int abs_ref(int x) {
+    return x < 0 ? -x : x;
+}
+
+int abs_bits(int x) {
+  int m = x>>31;
+  return (x^m) + ~m + 1;
+
+}
+
+int main() {
+    int t = rand();
+    int ar = abs_ref(t);
+    int ab = abs_bits(t);
+    int err = ar != ab;
+    assert(!err);
+}
