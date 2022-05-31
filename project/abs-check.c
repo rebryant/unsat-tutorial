34dbc161c71858b85f4ef0f7ab3705486c4b1042
Randal Bryant <Randy.Bryant@cs.cmu.edu>
2022-05-31 10:12:38 -0400

    Add PDFs

--- project/abs-check.c
+++ project/abs-check.c
@@ -1,17 +1,23 @@
-int abs_ref(int x) {
+#include <stdint.h>
+
+
+
+int32_t abs_ref(int32_t x) {
     return x < 0 ? -x : x;
 }
 
-int abs_bits(int x) {
-  int m = x>>31;
-  return (x^m) + ~m + 1;
+int32_t abs_bits(int32_t x) {
+    int32_t m = x>> (sizeof(int32_t) * 8 - 1);
+    return (x^m) + ~m + 1;
 
 }
 
-int main() {
-    int t = random();
-    int ar = abs_ref(t);
-    int ab = abs_bits(t);
-    int err = ar != ab;
+int32_t arbitrary();
+
+int32_t main() {
+    int32_t t = arbitrary();
+    int32_t ar = abs_ref(t);
+    int32_t ab = abs_bits(t);
+    int32_t err = ar != ab;
     assert(!err);
 }
